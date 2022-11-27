cdd20f4b75d38c8c501d21ebc3f862322c2e4296
Dizrtt <demonic2003@bk.ru>
2022-11-27 21:29:00 +0200

    Homework task 66 solved.

---
+++ HW2_Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.WriteLine("Please enter first number:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter last number:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+{
+    Console.WriteLine("The first and last numbers have been switched:");
+    Console.Write($"{n}, {m} ->");
+    SumBetweenNumbers(n, m, 0);
+}
+
+else
+{
+    Console.Write($"{m}, {n} ->");
+    SumBetweenNumbers(m, n, 0);
+}
+
+void SumBetweenNumbers(int start, int end, int sum)
+{
+    sum += end;
+    if (end <= start)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    SumBetweenNumbers(start, end - 1, sum);
+}
+
